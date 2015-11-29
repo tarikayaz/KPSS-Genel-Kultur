1fbb1cf352f5679131a7cd9fcd7ce621c902c459
İlim Turan <ilimturan@hotmail.com>
2015-11-29 04:29:18 +0200

    Create dev branch

--- app/src/main/java/com/zargidigames/kpssgenelkultur/api/ApiConfig.java
+++ app/src/main/java/com/zargidigames/kpssgenelkultur/api/ApiConfig.java
@@ -3,7 +3,7 @@ package com.zargidigames.kpssgenelkultur.api;
 public class ApiConfig {
 
     public final static String API_BASE_URL = "http://zargidigames.com/index.php/kpssapiandroid/";
-    public final static String API_KEY = "XXXXX";
-    public final static String API_CILENT_ID = "XXXXX";
+    public final static String API_KEY = "XXXXXX";
+    public final static String API_CILENT_ID = "XXXXXX";
 
 }
